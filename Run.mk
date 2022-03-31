284c4c1f92505855edd14ab663186270376b3432
jianxff <3109441270@qq.com>
2022-03-31 09:13:00 +0800

    Run project makefile.

---
+++ Run.mk
@@ -0,0 +1,61 @@
+NAME = main-service
+
+WORK_DIR 	= $(shell pwd)
+SRC_DIR  	= $(WORK_DIR)/src
+BUILD_DIR 	= $(WORK_DIR)/build
+INCLUDE_DIR = $(WORK_DIR)/include
+PACKAGE 	= $(BUILD_DIR)/$(NAME) 
+LIB			= $(BUILD_DIR)/libne.a
+AR 			= ar
+CXXFLAGS += $(addprefix -I , $(INCLUDE_DIR)) -O2 -Wall -Werror
+
+MODULES = $(filter-out src,$(notdir $(shell find $(SRC_DIR) -type d ) ) )
+
+OBJ_DIR = $(BUILD_DIR)/$(NAME)-obj
+
+ifeq ($(MODS),)
+SOURCES = $(shell find src -name "*.cpp" ! -name "*main.cpp")
+else 
+SOURCES = $(shell find $(addprefix src/,$(MODS)) -name "*.cpp" ! -name "*main.cpp")
+endif
+
+
+ifeq ($(MODE),debug)
+SOURCES += src/main.cpp
+else
+SOURCES += src/main.cpp
+endif
+
+OBJS = $(subst cpp,o,$(subst src,$(OBJ_DIR),$(SOURCES)))
+
+include scripts/colors.mk
+
+root : .detect $(LIB) 
+	@echo "$(C_GREEN)[$(shell date +%F%n%T)] Compile Complete!$(C_END)"
+
+$(OBJ_DIR)/%.o: $(SRC_DIR)/%.cpp
+	@echo + CXX $<
+	@mkdir -p $(dir $@)
+	@$(CXX) $(CXXFLAGS) -c -o $@ $<
+
+$(PACKAGE): $(OBJS)
+	@echo + LD $@
+	@$(CXX) $(CXXFLAGS) -o $@ $(OBJS)
+
+.PHONY: clean .detect run root
+
+run : .detect $(PACKAGE) 
+	@echo  "$(C_BLUE)[$(shell date +%F%n%T)] RUNNING...$(C_END)"
+	@$(PACKAGE) $(MAINARGS)
+	@echo  "$(C_GREEN)[$(shell date +%F%n%T)] SUCCESSFUL RUNNING$(C_END)"
+
+
+.detect :
+ifeq ($(MODE),debug)
+	@echo "$(C_YELLOW)Debug mode start up$(C_END)"
+endif
+	@echo "$(C_BLUE)Module <$(MODULES)> has been detected...$(C_END)"
+
+
+clean :
+	-rm -rf $(BUILD_DIR)
