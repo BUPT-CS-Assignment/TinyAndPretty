261cfb468719eda1aa19d4bad1d675567219cee5
jianxff <3109441270@qq.com>
2022-04-17 07:38:29 +0800

    For Terminal Mode.

--- src/main.cpp
+++ src/main.cpp
@@ -27,19 +27,3 @@ int main(/*int argv , char* argc[]*/){
     db.close();
     */
 }
-
-/*
-测试数据  @  
-CREATE test(ID:INT,NAME:TEXT,SCHOOL:LONGTEXT,KEY(ID));
-INSERT test(1001,Name_01,School_01);
-INSERT test(1002,Name_02,School_02);
-INSERT test(1003,Name_03,School_03);
-INSERT test(1004,Name_04,School_04);
-INSERT test(1005,Name_05,School_05);
-INSERT test(1006,Name_06,School_06);
-INSERT test(1007,Name_07,School_07);
-PRINT test;
-DELETE test(0);
-REMOVE test;
-
-*/
