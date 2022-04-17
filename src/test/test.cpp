61ae701b2f608daf11e0b19af8d4ffaca712416a
jianxff <3109441270@qq.com>
2022-04-17 09:24:01 +0800

    DataBase Test Function Added.

--- src/test/test.cpp
+++ src/test/test.cpp
@@ -1,15 +1,16 @@
 #include <test/define.h>
+#include <libs/NEdb.h>
 
 
-def_HttpEntry(Link_Start) {
+def_HttpEntry(Link_Start){
     std::cerr << "FUNC START\n";
     std::string t = "";
-    try {
-        auto& ans = request.queryForm("BUG FILE");
-    } catch (const HttpException& e) {
-        switch (e)
-        {
-            case HttpException::NON_POS :
+    try{
+        auto &ans = request.queryForm("BUG FILE");
+    }
+    catch(const HttpException &e){
+        switch(e){
+            case HttpException::NON_POS:
                 t += "OMG , I FAIL!";
                 break;
             case HttpException::NON_FORM:
@@ -29,12 +30,37 @@ def_HttpEntry(Link_Start) {
 }
 
 
-def_HttpEntry(Lent_Book) {
+def_HttpEntry(Lent_Book){
     std::cerr << "Start Lent\n";
 
-    std::fstream book("utils/out" , std::ios::in | std::ios::binary);
+    std::fstream book("utils/out", std::ios::in | std::ios::binary);
     if(book.is_open()) std::cerr << "Find IT!\n";
     std::cerr << "Send Back\n";
 
     return new FileResponse{book , "application/pdf"};
+}
+
+def_HttpEntry(DataBase_Test){
+    std::cerr << "Start DataBase Test\n";
+    std::string t = "";
+    NEdb DB("/home/jianxf/.nesrc");
+    if(!DB.dirInit()){
+        t = DB.getMsg();
+    }
+    else{
+        if(!DB.exec("create table tap_test(id int,name text,email text);\
+                    insert into tap_test values (0,user1,email_test1@tap.com);\
+                    insert into tap_test values (1,user2,email_test2@tap.com);\
+                    select * from tap_test;\
+        ")){
+            t = DB.getMsg();
+        }else{
+            t = DB.getData();
+        }
+        std::cerr<<DB.getMsg()<<std::endl;
+        DB.exec("drop table tap_test;");
+    }
+    DB.close();
+    return new HttpResponse{t};
+
 }
